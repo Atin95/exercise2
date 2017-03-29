bb00fa2b37216f82d981b741456a42c022543cb1
Adam Michniewski <adam.michniewski95@gmail.com>
2017-03-29 14:07:33 +0200

    Homework - Ćwiczenie procon

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,28 @@
 package wdsr.exercise2.procon;
 
+import java.util.LinkedList;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
-public class BufferManualImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+public class BufferManualImpl implements Buffer
+{
+	private LinkedList<Order> ll = new LinkedList<>();
+	
+	public void submitOrder(Order order) throws InterruptedException
+	{
+		synchronized(ll)
+		{
+			ll.add(order);
+		}
 	}
 	
-	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+	public synchronized Order consumeNextOrder() throws InterruptedException 
+	{
+		synchronized (ll) 
+		{
+			return ll.removeFirst();
+		}
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,21 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.*;
+
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
-public class BufferQueueImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+public class BufferQueueImpl implements Buffer 
+{
+	private BlockingQueue<Order> bq = new LinkedBlockingQueue<>();
+	
+	public void submitOrder(Order order) throws InterruptedException 
+	{
+		bq.put(order);
 	}
 	
-	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+	public Order consumeNextOrder() throws InterruptedException 
+	{
+		return bq.take();
 	}
 }
